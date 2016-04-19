c214d0605f537489e03375b717047b2c7e76542b
Steffen Imhof <github@steffen-imhof.de>
2016-04-19 22:37:03 +0200

    Fix language handling
    Parameter was not taken from node configuration.

--- node-red-contrib-forecast/forecast.js
+++ node-red-contrib-forecast/forecast.js
@@ -24,6 +24,7 @@ module.exports = function (RED) {
     this.units = config.units;
     this.latitude = config.latitude;
     this.longitude =  config.longitude;
+	this.language = config.language;
     
     forecast = new Forecast({
       service: 'forecast.io',
